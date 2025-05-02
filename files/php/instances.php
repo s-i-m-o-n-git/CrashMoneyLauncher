b39eb9477da06204a0ab41e414d473a097acf4da
Sim <70428699+s-i-m-o-n-git@users.noreply.github.com>
2025-05-02 17:03:31 +0200

    Update instances.php

--- files/php/instances.php
+++ files/php/instances.php
@@ -1,4 +1,6 @@
 <?php
+if (!isset($instance['1.8.9'])) $instance['1.8.9'] = [];
+
 $instance['1.8.9'] = array_merge($instance['1.8.9'], array(
     "loadder" => array(
         "minecraft_version" => "1.8.9",
@@ -26,6 +28,8 @@ $instance['1.8.9'] = array_merge($instance['1.8.9'], array(
         "port" => 25565
     )
 ));
+
+if (!isset($instance['CrashMoney'])) $instance['CrashMoney'] = [];
 
 $instance['CrashMoney'] = array_merge($instance['CrashMoney'], array(
     "loadder" => array(
