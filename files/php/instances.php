cb43fcfce8ec82e6b3b4aa4080658160f5229fa9
Sim <70428699+s-i-m-o-n-git@users.noreply.github.com>
2025-05-03 23:14:45 +0200

    Update instances.php

--- files/php/instances.php
+++ files/php/instances.php
@@ -1,34 +1,4 @@
 <?php
-if (!isset($instance['1.8.9'])) $instance['1.8.9'] = [];
-
-$instance['1.8.9'] = array_merge($instance['1.8.9'], array(
-    "loadder" => array(
-        "minecraft_version" => "1.8.9",
-        "loadder_type" => "forge",
-        "loadder_version" => "latest"
-    ),
-    "verify" => true,
-    "ignored" => array(
-        'config',
-        'essential',
-        'logs',
-        'resourcepacks',
-        'saves',
-        'screenshots',
-        'shaderpacks',
-        'W-OVERFLOW',
-        'options.txt',
-        'optionsof.txt'
-    ),
-    "whitelist" => array(),
-    "whitelistActive" => false,
-    "status" => array(
-        "nameServer" => "Hypixel",
-        "ip" => "mc.hypixel.net",
-        "port" => 25565
-    )
-));
-
 if (!isset($instance['CrashMoney'])) $instance['CrashMoney'] = [];
 
 $instance['CrashMoney'] = array_merge($instance['CrashMoney'], array(
@@ -43,8 +13,8 @@ $instance['CrashMoney'] = array_merge($instance['CrashMoney'], array(
     "whitelistActive" => false,
     "status" => array(
         "nameServer" => "CrashMoney",
-        "ip" => "mchub.com",
-        "port" => 25565
+        "ip" => "163.5.107.48",
+        "port" => 25500
     )
 ));
 ?>
